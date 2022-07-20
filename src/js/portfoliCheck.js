c2c77b03f1462dcc8631af35906f1d44d320bd00
Alex Rogach <alex.rogach131@ukr.net>
2022-07-20 22:16:32 +0300

    finish

--- src/js/portfoliCheck.js
+++ src/js/portfoliCheck.js
@@ -19,24 +19,32 @@ function portfoliCheck() {
            });
 
            if (item.innerHTML == startValues[index] && oneAction == 0) {
-
-            let link = item.querySelector('a').getAttribute('href'),
-                name = item.querySelector('a').getAttribute('name');
+            
+            const hash = window.location.hash.substr(1);
+            const arrTextUa = ["Підтвердьте перехід на ","Переходжу","Ні"];
+            const arrTextEn = ["Confirm the transition to ","Go to","No"];
+            let link = item.querySelector('a').getAttribute('href');
+            let name = item.querySelector('a').getAttribute('name');
+            let  arrTextUse = arrTextUa;
 
                 item.innerHTML = '';
-            
+
+                if(hash == "en") {
+                    arrTextUse = arrTextEn;
+                }
+
                 item.innerHTML = `
                     <div class="question">
-                        <p>Подтвердите переход на ${name}:</p>
-                        <button><a href="${link}">Уверен</a></button>
-                        <button>Сомневаюсь</button>
+                        <p>${arrTextUse[0]}${name}:</p>
+                        <button><a href="${link}">${arrTextUse[1]}</a></button>
+                        <button>${arrTextUse[2]}</button>
                     </div>
                 `;
 
                 item.querySelectorAll('.question button').forEach(btn => {
                     btn.addEventListener('click', () => {
 
-                        if (btn.textContent == 'Сомневаюсь') {
+                        if (btn.textContent == `${arrTextUse[2]}`) {
                             item.innerHTML = startValues[index];
                             oneAction++;
                         } 
